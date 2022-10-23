762761ae08b6349e6f222c6466ee7ae95cb9efb5
sametkykc <samet.kayikci@birlesikodeme.com>
2022-10-23 15:30:29 +0300

    Employee Class Add Surname Properties

--- PaparaFirstWeek/PaparaFirstWeek/Model/Employee.cs
+++ PaparaFirstWeek/PaparaFirstWeek/Model/Employee.cs
@@ -8,6 +8,7 @@ namespace PaparaFirstWeek.Model
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string Surname { get; set; }
         public string Gender { get; set; }
         public string City { get; set; }
         public int Age { get; set; }
